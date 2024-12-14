e8012df14611a63b19268e6a9dd25b255276e4e3
KeshavK <keshavsign8@gmail.com>
2024-12-14 19:39:37 +0530

    added-all script again

--- catalogue.sh
+++ catalogue.sh
@@ -83,6 +83,6 @@ dnf install mongodb-org-shell -y &>>$LOGFILE
 
 VALIDATE $? " install mongodb-client"
 
-mongo --host mongodb.roboshop.online </app/schema/catalogue.js &>>$LOGFILE
+mongo --host mongodb.joindevops.online </app/schema/catalogue.js &>>$LOGFILE
 
 VALIDATE $? "Load Schema"
